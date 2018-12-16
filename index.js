31a1c72da2bc39f88ca8d609e1461483eceb41f8
Grzegorz Sowa <sowa.gregory@gmail.com>
2018-12-16 20:12:10 +0100

    multi sessions

--- index.js
+++ index.js
@@ -1,85 +1,139 @@
 const net = require("net");
-const readline = require('readline');
+const readline = require("readline");
 
-const rl = readline.createInterface(
-{
+const rl = readline.createInterface({
   input: process.stdin,
-  output:process.stdout
+  output: process.stdout
 });
 
 var server = new net.Server();
+var sessions_dict = {};
 
-var sessions = {};
-var session_number = 1;
-var current_session = 1;
+// number of all connected sessions (might already be disconnected)
+// each connected session has its own unique identifier
+var max_session_number = 1;
 
-Reset = "\x1b[0m"
-Bright = "\x1b[1m"
-Dim = "\x1b[2m"
-Underscore = "\x1b[4m"
-Blink = "\x1b[5m"
-Reverse = "\x1b[7m"
-Hidden = "\x1b[8m"
+// number of session to which commands from input line will be send
+var active_session = 0;
 
-FgBlack = "\x1b[30m"
-FgRed = "\x1b[31m"
-FgGreen = "\x1b[32m"
-FgYellow = "\x1b[33m"
-FgBlue = "\x1b[34m"
-FgMagenta = "\x1b[35m"
-FgCyan = "\x1b[36m"
-FgWhite = "\x1b[37m"
-
-
-
+Reset = "\x1b[0m";
+FgRed = "\x1b[31m";
+FgGreen = "\x1b[32m";
+FgYellow = "\x1b[33m";
+FgBlue = "\x1b[34m";
+FgWhite = "\x1b[37m";
 
 server.on("connection", function(socket) {
-    
   console.log(socket.remoteAddress);
-  var session = session_number;
-  sessions[session_number++] = socket;
+  let session_id = max_session_number++;
+  sessions_dict[session_id] = socket;
 
-  printSessionPrefix(FgGreen, current_session);
-  console.log(socket.remoteAddress+" "+socket.remotePort );
-    
-  printInputReady(session);
+  printSessionPrefix(FgGreen, session_id);
+  process.stdout.write(socket.remoteAddress + " " + socket.remotePort + "\n");
+
+  // if no active sessions - connected session becomes active session_id
+  if (active_session == 0) active_session = session_id;
+
   socket.on("data", data => {
-    receivedData(session, data);
+    receivedData(session_id, data);
   });
   socket.on("close", () => {
-    connClosed(session);
+    connClosed(session_id);
   });
   socket.on("error", () => {
-    connClosed(session);
+    connError(session_id);
   });
 });
 
-rl.on("line", (line)=>
-{
-  console.log("#"+line);
+rl.on("line", line => {
+  if (active_session == 0) {
+    printInputReady();
+    return;
+  }
+
+  // no special command in input line
+  if (!line.startsWith("@")) {
+    sessions_dict[active_session].write(line);
+    return;
+  }
+
+  if (line === "@") {
+    // list all sessions
+    let keys = Object.keys(sessions_dict);
+    process.stdout.write("Existing sessions:");
+    for (let index in keys) {
+      if (index > 0) process.stdout.write(" ");
+      process.stdout.write("[" + keys[index] + "]");
+    }
+    process.stdout.write("\n");
+  } else {
+    // special command - starts with @ provided in input line
+    let temp_id = parseInt(line.substr(1));
+    if (isNaN(temp_id)) console.log("Invalid session number");
+    else if (temp_id in sessions_dict) active_session = temp_id;
+    else console.log("No session number:" + temp_id + " exists");
+  }
   printInputReady();
 });
 
-function printInputReady()
-{
-    printSessionPrefix( FgBlue, current_session);
-    process.stdout.write( Bright );
+function printInputReady() {
+  printSessionPrefix(FgBlue, active_session);
 }
 
-function printSessionPrefix( color, session_number)
-{
-    process.stdout.write(color+"["+session_number.toString() + "]  ");
+function printSessionPrefix(color, session_id) {
+  if (session_id == 0) process.stdout.write(color + "[-no-sessions-] ");
+  else process.stdout.write(color + "[" + session_id.toString() + "] ");
 }
 
-function receivedData(session_number, data) {
-    printSessionPrefix(FgWhite, session_number);
-    process.stdout.write( Bright+data.toString());
-    printInputReady();
+function receivedData(session_id, data) {
+  let lines = data.toString().split("\n");
+  for (let index = 0; index < lines.length; index++) {
+    printSessionPrefix(FgYellow, session_id);
+    // lines contains '\r' char inside - caret return, which might cause side effects
+    let line = lines[index].replace("\r", "");
+    process.stdout.write(line);
+    process.stdout.write("\n");
+  }
+  printInputReady();
 }
 
-function connClosed(session_number) {
-  var prefix = "[" + session_number.toString() + "]";
-  console.log(prefix + "disconn");
+function connError(session_id) {
+  process.stdout.write("\n");
+  printSessionPrefix(FgRed, session_id);
+  process.stdout.write("session error\n");
 }
 
-server.listen(20000);
+function findNextSession(session_id) {
+  if (session_id == undefined) throw new Error("session_id is undefined");
+
+  // no more existing sessions
+  if (Object.keys(sessions_dict).length == 0) return 0;
+
+  // try to find next larger session number, than currently active
+  for (let i = session_id; i < max_session_number; i++) {
+    if (i in sessions_dict) return i;
+  }
+
+  // try to find available session_id
+  for (let i = 1; i < session_id; i++) {
+    if (i in sessions_dict) return i;
+  }
+  throw new Error("sth went wrong");
+}
+es;
+
+function connClosed(session_id) {
+  printSessionPrefix(FgRed, session_id);
+  process.stdout.write("session disconnected\n");
+  delete sessions_dict[session_id];
+  active_session = findNextSession(session_id);
+  printInputReady();
+}
+
+const port = 10000;
+server.listen(port);
+console.log("Reverse Shell Server v1.0 - listening on port:" + port);
+console.log(
+  "CmdLine - enter:\n@ - to list all connected sessions\n@n - to switch to session number n\n\n"
+);
+printInputReady();
